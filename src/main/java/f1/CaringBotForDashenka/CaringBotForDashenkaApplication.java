2754d006b43921842667dfae42cffe623a1e3a6c
KhiromuElrick <khiromuelrick@gmail.com>
2022-09-11 16:49:27 +0300

    Initial commit

---
+++ src/main/java/f1/CaringBotForDashenka/CaringBotForDashenkaApplication.java
@@ -0,0 +1,13 @@
+package f1.CaringBotForDashenka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaringBotForDashenkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaringBotForDashenkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/f1/CaringBotForDashenka/CaringBotForDashenkaApplicationTests.java
@@ -0,0 +1,13 @@
+package f1.CaringBotForDashenka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaringBotForDashenkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
