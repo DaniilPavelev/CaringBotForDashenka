b509df56fac805c45ea2c29256fead3175c3c7b3
KhiromuElrick <khiromuelrick@gmail.com>
2022-09-11 17:45:23 +0300

    тест2

--- src/main/java/f1/CaringBotForDashenka/service/TelegramBot.java
+++ src/main/java/f1/CaringBotForDashenka/service/TelegramBot.java
@@ -4,9 +4,15 @@ import f1.CaringBotForDashenka.config.BotConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
+import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Slf4j
@@ -17,6 +23,21 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     public TelegramBot(BotConfig config) {
         this.config = config;
+        List<BotCommand> listOfCommands = new ArrayList<>();
+        listOfCommands.add(new BotCommand("/start", "Начни получать свою заботу..."));
+        listOfCommands.add(new BotCommand("/aboutMe", "Немного о том для кого и зачем я был создан"));
+        listOfCommands.add(new BotCommand("/whatWeather","Расскажу какая погода в твоём городе"));
+        listOfCommands.add(new BotCommand("/whatToWear","Расскажу тебе как одеться по погоде, чтоб не заболеть"));
+        listOfCommands.add(new BotCommand("/wordsOfCare","Всем хочется получить немного заботы"));
+        listOfCommands.add(new BotCommand("/settings","подкрутить штуки"));
+        listOfCommands.add(new BotCommand("/showCat","Котички!"));
+        listOfCommands.add(new BotCommand("/showDog","Собачки!"));
+        try{
+            this.execute(new SetMyCommands(listOfCommands, new BotCommandScopeDefault(),null));
+        }
+        catch (TelegramApiException e){
+            log.error("Error menu list"+e.getMessage());
+        }
     }
 
     @Override
@@ -47,7 +68,7 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     private void srartCommandReceived(long chatId, String name){
-        String answer = "Hi, " + name+ ", nice cooc";
+        String answer = "Hi, " + name+ ", nice man";
         log.info("Replied to user " + name );
         sendMessage(chatId, answer);
     }
